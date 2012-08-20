993fbe06f7d414f2dc25448f8c3175455d5e1472
Artem Smirnov <ac.hedgehog@gmail.com>
2012-08-20 19:00:35 +0200

    env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TicTacToe::Application.initialize!
+
+require 'hassle'
+
+Rails::Initializer.run do |config|
+  config.middleware.use Hassle
+end
