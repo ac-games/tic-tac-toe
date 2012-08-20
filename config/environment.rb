bd8fcf8a22dd0d391f863099e73fc39128c21abb
Artem Smirnov <ac.hedgehog@gmail.com>
2012-08-20 19:10:00 +0200

    remove hassle 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TicTacToe::Application.initialize!
-
-require 'hassle'
-
-Rails::Initializer.run do |config|
-  config.middleware.use Hassle
-end
